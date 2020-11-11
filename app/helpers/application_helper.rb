544f349421e601c7ec0c04e32bf5c1917c57e824
clousky2020 <464901210@qq.com>
2020-11-11 20:03:31 +0800

    更改页面表现形式

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,13 @@ module ApplicationHelper
     Time.current.localtime.strftime("%Y-%m-%d")
   end
 
+  # 根据订单是否通过审核，改变框内的颜色
+  def check_status_css(object)
+    if !object.check_status
+      status = "alert-danger"
+    else
+      status = "alert-success"
+    end
+  end
+
 end
