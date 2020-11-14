36a19e824519cf4fc61203fff328a826db55aff3
clousky2020 <464901210@qq.com>
2020-11-14 12:01:53 +0800

    添加改机记录的机器编号搜索

--- app/models/change_machine.rb
+++ app/models/change_machine.rb
@@ -5,7 +5,7 @@ class ChangeMachine < ApplicationRecord
 
   def self.search(search)
     if search
-      where("change_person LIKE :term or contacts LIKE :term or  machine_type LIKE :term or  change_to_specification LIKE :term or remark LIKE :term ", term: "%#{search}%")
+      where("change_person LIKE :term or contacts LIKE :term or  machine_type LIKE :term or  change_to_specification LIKE :term or remark LIKE :term or machine_id = :term_i ", term: "%#{search}%", term_i: search.to_i)
     else
       all
     end
