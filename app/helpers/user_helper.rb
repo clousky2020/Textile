4ae3931ef789999ad649aa86db441ea3cf5aa8e1
clousky2020 <464901210@qq.com>
2020-11-15 10:28:10 +0800

    个人信息界面查看报销单情况

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,12 @@
 module UserHelper
 
-  def role_to_chinese(en)
-    roles = ['admin' => "管理员", 'leader' => "领导", 'editor' => "编辑员", 'employee' => "员工"]
-    out = roles[0][en]
+  # 根据订单是否通过报销，改变框内的颜色
+  def order_status_css(object)
+    if object.reimbursed
+      "alert-success"
+    else
+      "alert-warning"
+    end
   end
+
 end
