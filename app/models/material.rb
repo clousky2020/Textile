5b68c77b3e7c2a1aa1033e80cc9b7d37a5a5b526
clousky2020 <464901210@qq.com>
2020-11-18 17:26:53 +0800

    把原料唯一性扩大到公司，不同公司可以有相同的原料

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,7 +2,7 @@ class Material < ApplicationRecord
   belongs_to :purchase_supplier
   has_many :purchase_orders
 
-  validates :name, presence: true, uniqueness: {scope: :specification}
+  validates :name, presence: true, uniqueness: {scope: [:specification, :purchase_supplier_id]}
   validates :name, :specification, presence: true
 
   def self.search(search)
